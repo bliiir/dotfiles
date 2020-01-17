c1603644ecf310fca975d7c32e2bcef782184bef
bliiir <bliiir@users.noreply.github.com>
2020-01-17 21:30:36 +0100

    Update python.sh

--- .local/bin/install/python.sh
+++ .local/bin/install/python.sh
@@ -12,7 +12,7 @@ pip3 install numpy matplotlib scipy pandas seaborn sklearn
 ########################################
 # other
 ########################################
-pip3 install pytest thefuck pyenv
+pip3 install pytest thefuck
 
 ########################################
 # powerline (tmux and zsh dependency)
@@ -22,3 +22,11 @@ if [[ $OS == "Linux" ]]; then
 fi
 pip install --user powerline-status
 pip3 install --user powerline-status
+
+########################################
+# pyenv
+########################################
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
+echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bash_profile
