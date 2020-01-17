8ec4582535009a1db32b53558d51feb5126b501e
bliiir <bliiir@users.noreply.github.com>
2020-01-17 20:31:42 +0100

    Update macos_dependencies.sh

--- .local/bin/install/macos_dependencies.sh
+++ .local/bin/install/macos_dependencies.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #install OSX dependencies
-brew install git curl tmux node cmake
+brew install git curl iterm2 tmux node cmake 
 
 # iperl dependencies
 brew install zeromq libmagic
@@ -9,20 +9,20 @@ brew install zeromq libmagic
 # gnu tools:
 brew install coreutils
 brew tap homebrew/dupes 
-brew tap homebrew/science
+# brew tap homebrew/science
 brew install transpose
-brew install binutils diffutils gawk wget gzip screen watch gnutls
-brew install ed --with-default-names
-brew install findutils --with-default-names
-brew install gnu-indent --with-default-names
-brew install gnu-sed --with-default-names
-brew install gnu-tar --with-default-names
-brew install gnu-which --with-default-names
+#brew install binutils diffutils gawk wget gzip screen watch gnutls
+# brew install ed --with-default-names
+# brew install findutils --with-default-names
+# brew install gnu-indent --with-default-names
+# brew install gnu-sed --with-default-names
+# brew install gnu-tar --with-default-names
+# brew install gnu-which --with-default-names
 brew install grep --with-default-names
-brew install wdiff --with-gettext
+# brew install wdiff --with-gettext
 
 # other
-brew install jq
+# brew install jq
 
 # OSX specific stuff
 wget https://www.iterm2.com/utilities/imgcat -P ~/.local/bin
