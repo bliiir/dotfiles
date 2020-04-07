b6a69ebcfe19252cdff49b07d9122ef48523c4a2
rg <rasgroth@gmail.com>
2020-04-07 10:37:55 +0200

    Removed .local

--- .local/bin/install/zsh.sh
+++
@@ -1,6 +0,0 @@
-
-touch $HOME/.config/zsh/extra.zsh
-########################################
-# antigen (zsh extension manager)
-########################################
-curl https://cdn.rawgit.com/zsh-users/antigen/v1.4.0/bin/antigen.zsh > $HOME/.local/bin/antigen.zsh
